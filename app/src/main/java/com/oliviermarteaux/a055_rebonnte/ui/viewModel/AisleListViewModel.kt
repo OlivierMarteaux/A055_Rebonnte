eac22b9bd59ea45d0fc11c4cf4695cc641a1122c
Olivier Marteaux <olivier_marteaux@hotmail.com>
2026-01-25 13:01:19 +0100

    load data in init on AisleListScreen only in tests

--- app/src/main/java/com/oliviermarteaux/a055_rebonnte/ui/viewModel/AisleListViewModel.kt
+++ app/src/main/java/com/oliviermarteaux/a055_rebonnte/ui/viewModel/AisleListViewModel.kt
@@ -116,7 +116,10 @@ class AisleListViewModel @Inject constructor(
         log.d("AisleListViewModel: init")
 
         // Sign in the test user in case of test config
-        if (TestConfig.isTest) signInTestUser()
+        if (TestConfig.isTest) {
+            signInTestUser()
+            loadFirstPage()
+        }
 
         // fixed: do not load in init as it leads to auth error when instantiated on SplashScreen
 //        loadFirstPage()
